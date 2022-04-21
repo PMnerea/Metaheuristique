e3f54f4d3faac1159328b6aeaaf37c75b9a19645
Pruneau-Marin Nerea <pruneau-@insa-11268>
2022-04-21 12:16:52 +0200

    descent solver

--- src/main/java/jobshop/solvers/DescentSolver.java
+++ src/main/java/jobshop/solvers/DescentSolver.java
@@ -1,11 +1,13 @@
 package jobshop.solvers;
 
 import jobshop.Instance;
+import jobshop.encodings.ResourceOrder;
 import jobshop.encodings.Schedule;
 import jobshop.encodings.Task;
 import jobshop.solvers.neighborhood.Neighborhood;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /** An empty shell to implement a descent solver. */
@@ -28,7 +30,32 @@ public class DescentSolver implements Solver {
     // TODO - Implement a descent solver using Nowicki and Smutnicki neighborhood
     @Override
     public Optional<Schedule> solve(Instance instance, long deadline) {
-        throw new UnsupportedOperationException();
+        Optional<Schedule> schedule = this.baseSolver.solve(instance, deadline);
+
+        boolean foundSolution = false;
+        while (!foundSolution) {
+            List<ResourceOrder> neighborhood = this.neighborhood.generateNeighbors(new ResourceOrder(schedule.get()));
+            int bestNeighbor = -1;
+            int bestNeighborSpan = -1;
+            int bestMakespan = schedule.get().makespan();
+
+            for (int i=0; i<neighborhood.size(); i++) {
+                int currentSpan = neighborhood.get(i).toSchedule().get().makespan();
+                if (currentSpan < bestNeighborSpan || bestNeighborSpan == -1) {
+                    bestNeighborSpan = currentSpan;
+                    bestNeighbor = i;
+                }
+            }
+
+            if (bestNeighborSpan >= bestMakespan) {
+                foundSolution = true;
+            }
+            else {
+                schedule = neighborhood.get(bestNeighbor).toSchedule();
+            }
+        }
+
+        return schedule;
     }
 
 }
